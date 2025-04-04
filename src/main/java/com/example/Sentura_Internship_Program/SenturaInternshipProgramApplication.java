c6c99ed06703df7b95d0533a305a360b85a35728
kavindu0818 <kavindumaduranga184@gamil.com>
2025-04-04 09:02:11 +0530

    first commite

---
+++ src/main/java/com/example/Sentura_Internship_Program/SenturaInternshipProgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sentura_Internship_Program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenturaInternshipProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenturaInternshipProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sentura_Internship_Program/SenturaInternshipProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sentura_Internship_Program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenturaInternshipProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
